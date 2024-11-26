a2cb3a166deaa71e86f95366f91b30e57cf7a06a
Eric Bidopa <ericbidopa@gmail.com>
2024-11-26 10:58:31 +0000

    moving on to wrap my head around functionality

--- SpellingLessons/SpellingLessonsPage.js
+++ SpellingLessons/SpellingLessonsPage.js
@@ -1,15 +1,58 @@
-import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Pressable,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const SpellingLessonsPage = () => {
+  const [isPressed, setIsPressed] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const speakIsPressed = () => {
+    setIsPressed(true);
+    console.log("its pressed");
+    setIsPressed(false);
+  };
+  const submitBtnPressed = ()=>{
+    console.log("answer submitted")
+  }
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <View style={styles.wrapper}>
-            <View>
+          <Pressable onPress={speakIsPressed} style={styles.speakBtn}>
+            <Ionicons
+              name={isPressed == true ? "mic" : "mic-outline"}
+              size={80}
+              color="black"
+            />
+          </Pressable>
+          <View
+            style={[
+              styles.searchContainer,
+              isFocused && styles.searchContainerFocused,
+            ]}
+          >
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Spell the Word Here"
+              placeholderTextColor="gray"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+          </View>
 
-            </View>
-          <Text>This spelings page</Text>
+          <Pressable
+            style={styles.submitBtn}
+            onPress={submitBtnPressed}
+          >
+            <Text>Submit Answer</Text>
+          </Pressable>
         </View>
       </View>
     </SafeAreaView>
@@ -29,7 +72,43 @@ const styles = StyleSheet.create({
     // flex: 1,
   },
   wrapper: {
-    backgroundColor: "blue",
-    flex: 1
+    // backgroundColor: "blue",
+    flex: 1,
+    flexDirection: "column",
+    gap: 2,
+    paddingVertical: "5%",
+  },
+  speakBtn: {
+    padding: "30%",
+    backgroundColor: "pink",
+    borderRadius: 50,
+    alignSelf: "center",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderColor: "#f0f0f0",
+    marginVertical: 15,
+  },
+  searchContainerFocused: {
+    borderColor: "black", // Focused border color
+    borderWidth: 1.5,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 30,
+  },
+  submitBtn: {
+    padding: 17,
+    borderColor: "black",
+    borderWidth: 1,
+    alignItems: "center",
+    borderRadius: 10,
+    marginVertical: 10,
   },
 });
